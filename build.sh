709da2bd83ea7f09c58cd60d5daa17c8d99be122
Xuejie Xiao <xxuejie@gmail.com>
2023-08-02 04:30:22 +0000

    Use RUSTFLAGS to enable B extension instead of custom toolchain

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -ex
 
+
 cargo build --release --package tx-generator
-cargo build --release --package rust-baseline-verifier --target=riscv64imac_zba_zbb_zbc_zbs-unknown-ckb-elf
-cargo build --release --package rust-compiled-verifier --target=riscv64imac_zba_zbb_zbc_zbs-unknown-ckb-elf
+
+export RUSTFLAGS='-C target-feature=+zba,+zbb,+zbc,+zbs'
+cargo build --release --package rust-baseline-verifier --target=riscv64imac-unknown-none-elf
+cargo build --release --package rust-compiled-verifier --target=riscv64imac-unknown-none-elf
+unset RUSTFLAGS
 
 riscv64-ckb-elf-gcc c/ckb_mmr_entry.c -o c_verifier \
   -O3 -nostdlib -nostdinc -g \
